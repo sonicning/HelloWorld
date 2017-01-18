abc853c9be9c6afb03ea7b6c0104cc38d1a0102a
Sonic Ning <sonicning@gmail.com>
2017-01-18 20:02:32 +0800

    Create NewFeature2.java (#3)
    changed date format , commit again

--- NewFeature2.java
+++ NewFeature2.java
@@ -1,1 +1,14 @@
-//This is new feature 2 in master
+//This is new feature 2 test to be pulled into master
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class NewFeature2 {
+
+	public static void main(String[] args) {
+		// Auto-generated method stub
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");//设置日期格式  dateFormat changed as requested @sonic-developer1
+		System.out.println("This is new feature 2 test in a new branch");
+		System.out.println("The time is  " + df.format(new Date()));
+	}
+
+}
